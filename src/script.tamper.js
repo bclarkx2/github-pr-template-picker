aec721fe779e3295a3de372615e7aab18b60f7fc
Brian Clark <bclark@gmail.com>
2022-08-04 10:24:16 -0400

    Add tamper.js

---
+++ src/script.tamper.js
@@ -0,0 +1,120 @@
+// ==UserScript==
+// @name         GitHub PR Template Picker
+// @namespace    clarknet.info
+// @version      0.1.0
+// @description  Allows picking a changing the template for a PR in GitHub
+// @author       @bclarkx2
+// @match        https://github.com/*/*/compare/*
+// ==/UserScript==
+
+(async function () {
+  "use strict";
+
+  // Return early if script has already run
+  if (document.body.dataset.gprp) return;
+  document.body.setAttribute("data-gprp", true);
+
+  // Find selection control
+  const brancher = document.querySelector(
+    ".new-discussion-timeline .range-editor"
+  );
+
+  const holder = await picker();
+  console.log(holder);
+
+  // Add holder after brancher
+  brancher.parentNode.insertBefore(holder, brancher.nextSibling);
+})();
+
+// Handle a change event from the template picker.
+// @param {Event} e - from a select control
+function handleSelect(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const template = e.currentTarget.value;
+  setTemplate(template);
+}
+
+// Set the template parameter in the current page URL
+// @param {string} template - the name of the template file ('' to remove)
+function setTemplate(template) {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  if (template === "") {
+    urlParams.delete("template");
+  } else {
+    urlParams.set("template", template);
+  }
+
+  window.location.search = urlParams;
+}
+
+// Return the current template URL parameter
+// @returns {string}
+function currentTemplate() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("template");
+}
+
+// Build a PR template picker holder
+// @returns {HTMLElement}
+async function picker() {
+  // Create new template picker container
+  const holder = document.createElement("div");
+  holder.id = "prTemplateHolder";
+  holder.classList.add("range-editor");
+  holder.classList.add("color-fg-muted");
+  holder.classList.add("js-range-editor");
+
+  // Create the label
+  const label = document.createElement("label");
+  label.textContent = "PR Template: ";
+  holder.appendChild(label);
+
+  // Create the picker
+  const picker = document.createElement("select");
+  picker.id = "prTemplatePicker";
+
+  // TODO: do I need to await here?
+  const names = ["", ...(await templates())];
+  for (templateName of names) {
+    const option = document.createElement("option");
+    option.value = templateName;
+    option.text = templateName;
+    picker.appendChild(option);
+  }
+
+  picker.value = currentTemplate();
+
+  // Attach handler to picker
+  picker.addEventListener("change", handleSelect, true);
+
+  // Add the picker to the holder
+  holder.appendChild(picker);
+
+  return holder;
+}
+
+// Return the set of available template names
+// @returns {Promise}
+async function templates() {
+  return fetch(
+    "https://api.github.com/repos/nicheinc/.github/contents/.github/PULL_REQUEST_TEMPLATE"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response;
+    })
+    .then((response) => response.json())
+    .then((results) =>
+      results
+        .map((el) => el.name)
+        .filter((el) => el.endsWith(".md") && el !== "README.md")
+    )
+    .catch((error) => {
+      console.log(`can't fetch PR templates: ${error}`);
+      return [];
+    });
+}
